d82f92a15e8ab12c980eb19dc0c8c3823d898ab0
Hai Yen Nguyen <haiyenng411@gmail.com>
2024-03-08 10:24:45 +0700

    Buoi02

---
+++ src/main/java/com/sof3011/yennth4/B2_LamQuenVoiForm/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.sof3011.yennth4.B2_LamQuenVoiForm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+// Ctrl Alt O : để bỏ những import thừa
+
+import java.io.IOException;
+// value : Chua duong dan cua trang web
+// Duong dan BAT BUOC PHAI BAT DAU / => KHONG LA CHET TOMCAT K BUILD DC (J4 là bắt buộc , J5 thì không cần)
+// server vs client => HTTP methods :
+// Co 4 loai co ban:
+// GET : Hien thi lay ra (READ)
+// POST: Them xu ly (CREATE)
+// PUT: Update (UPDATE)
+// DELETE delete => Xoa (DELETE)
+// J4, J5 => CHI DUNG 2 LOAI =>
+// GET(Hien thi)
+// POST (Xu ly)
+// VALUE => Giong Y SI DE BAI
+// FE (view clien giao diẹn) => jframe/jsp/html => webapp
+// BE(server) => logic => service, repositor, ..
+//FE va BE : trao doi bang controller thong qua phuong thuc HTTPMethods
+
+@WebServlet(value = {"/login", "/ket-qua"})
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // truyen du lieu tu Servlet - sang JSP  (dùng set Attribute)
+        String message = "Hello World";
+        request.setAttribute("messageJSP", message);
+
+        // chuyen trang
+        // C1 : getRequestDispatcher :
+        request.getRequestDispatcher("form-login.jsp")
+                .forward(request, response);
+
+        // C2 : sendRedirect : chuyen trang thuan - khong lay duoc du lieu tu JSP
+        // response.sendRedirect("form-login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // lay du lieu nhap tu JSP sang Servlet =) getParamsmetter
+        String usname = request.getParameter("uname"); // lay gtri o input ( trong nhay kep la name JSP )
+        String password = request.getParameter("psw");
+        if (usname == null) {
+            request.setAttribute("validateUSName" , "Khong duoc de trong");
+        }
+        if (password == null) {
+            request.setAttribute("validatePassword" , "Khong duoc de trong");
+        }
+        if (usname != null && password != null) {
+            request.setAttribute("usname", usname);
+            request.setAttribute("password", password);
+        }
+        request.getRequestDispatcher("Buoi02/ket-qua.jsp")
+                .forward(request, response);
+    }
+}
---
+++ target/hangnt169-1.0-SNAPSHOT/Buoi02/ket-qua.jsp
@@ -0,0 +1,12 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Ket Qua</title>
+</head>
+<body>
+    <h2> Xin chào đây là tài khoản / mật khẩu của bạn </h2>
+    <b>Username : </b> ${usname} <br>
+    <b>Password : </b> ${password}
+</body>
+</html>
